e24145541115ceb2191fda1c17db68c88b80aa80
Sasha <sasha@example.com>
2022-10-02 07:42:58 +0300

    самма четных индексов

---
+++ task_36/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+int [] getArrayrandomNumber(int lenght, int start, int end)
+{
+    int[] resultArray = new int[lenght];
+    for (int i = 0; i < resultArray.Length ; i++)
+    {
+        resultArray[i] = new Random().Next(start, end);
+    }
+    return resultArray;
+}
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length)
+        {
+            Console.Write(",");
+        }
+       
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+
+}
+int counterEvenIndexNumber(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 1)
+        {
+            result += array[i];
+        }
+    }
+
+    return result;
+}
+
+int leghtarray = new Random().Next(3,20);
+int[] Spisok = getArrayrandomNumber(leghtarray,0, 99);
+PrintArray(Spisok);
+Console.WriteLine();
+int suumIndex = counterEvenIndexNumber(Spisok);
+Console.WriteLine($" Сумма элементов четных позиций = {suumIndex}");
+
+
+
